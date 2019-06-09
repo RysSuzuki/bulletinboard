171dbb73ee0eaff463538aedb4537f00966636af
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-06-09 12:53:26 +0900

    投稿機能、一覧表示機能追加

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,4 +1,29 @@
 class LettersController < ApplicationController
   def index
+    @letter = Letter.all
+  end
+
+  def new
+    if params[:back]
+      @letter = Letter.new(letter_params)
+    else
+      @letter = Letter.new
+    end
+  end
+
+  def create
+    @letter = Letter.new(letter_params)
+    if @letter.save
+      binding.pry
+     redirect_to action: 'index'
+    else
+     render 'index'
+    end
+  end
+
+  private
+
+  def letter_params
+        params.require(:letter).permit(:content)
   end
 end
